162dc2b989657e100a95a50fa0220f4d10ed5342
raghursk <108519569+raghursk@users.noreply.github.com>
2024-04-20 22:41:18 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,76 @@
+# import sys
+# print(sys.version)
+# if 5>2:
+#     print('not greater the that number:\n')
+#     # else
+#     print('not greater the that number:\n')
+# for i in range(5):
+#     print("hi")
+#     print("raghu")
+# """python in variables
+# we used here
+# """
+# print("hi")
+# name = input("enter the name:")
+# print(name)
+# print(len(name))
+
+
+
+# a=str(1)
+# b=str("raghu")
+# print(a+b)
+
+# a=5
+# b=10
+# print(a+b)
+
+
+# type of datatype
+# a=5
+# print(type(a))
+
+# def add(a,b):
+#     print(a+b)
+
+# a=5
+# b=5
+# add(a,b)
+# functions  in python 
+# a=10
+
+# #global variable 
+# x = "awesome"#global variable 
+# def myfunc():#define the function
+#   x = "fantastic"#x = is  variable 
+#   # x->data will be assine whole data...
+#   print("Python is " + x) # its the frist setatment
+
+# myfunc()#calling the function
+
+# print("Python is " + x)#secend setatment it  will executd the code
+
+# a=10
+# def num():
+#     print(a)
+#     def show():
+#         global a
+#         a = a + 1
+#         print(a)
+#         show()
+# num()
+
+
+
+# a = 10
+# def num():
+#     print(a)
+#     def show():
+#         global a
+#         a = a + 1
+#         print(a)
+#     show()  # Call the show() function here to increment and print 'a'
+# num()
+
+
+
